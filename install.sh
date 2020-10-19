3c4171bb4eb030c0da787922bcbecc8ba6b22db0
iyoet <iyoet@greentux.us.to>
2020-10-20 02:32:14 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-wget https://download.mikrotik.com/routeros/6.45.9/chr-6.45.9.img.zip
+wget https://download.mikrotik.com/routeros/6.46.7/chr-6.46.7.img.zip
 gunzip -S .zip *zip
 mv chr* chr.img
 dd if=chr.img of=/dev/sda
